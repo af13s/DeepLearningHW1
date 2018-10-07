538395b4897749170f2032dd8f1ad48c158590fe
Cameron <cameron.farzaneh@gmail.com>
2018-10-07 16:51:49 -0400

    Confusion matrix implemented

--- FNN_1.py
+++ FNN_1.py
@@ -7,7 +7,8 @@ from keras.layers import Dense, Dropout
 from keras.optimizers import Adam
 from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
-from TestTools import plot_results, show_misclassified
+from sklearn.metrics import confusion_matrix
+import itertools
 
 def plot_results(history, epochs):
 	plt.style.use("ggplot")
@@ -21,6 +22,38 @@ def plot_results(history, epochs):
 	plt.ylabel("Loss/Accuracy")
 	plt.legend(loc="upper left")
 	plt.show()
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+    
+    plt.rcParams.update(plt.rcParamsDefault)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
 
 def load_data():
 
@@ -101,3 +134,9 @@ if __name__ == "__main__":
 	print('Test accuracy:', score[1])
 
 	plot_results(history,epochs)
+
+	classes = ['0','1','2','3','4','5','6','7','8','9']
+	predictions = model.predict(testData).argmax(axis=-1)
+	trueOutputs = testLabels.argmax(axis=-1)
+	cm = confusion_matrix(trueOutputs,predictions)
+	plot_confusion_matrix(cm,classes)
